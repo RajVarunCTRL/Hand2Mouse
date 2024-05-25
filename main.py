5d5871ea4748a30fa1476b6cac529f987b5c5104
RajVarun <grajvarun@gmail.com>
2024-05-26 01:42:53 +0530

    Added fail safe mechanism

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import cv2
-import numpy
+import numpy as np
+import pyautogui
+from rich.console import Console
+
+console = Console()
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    # Region to capture
+    roc = frame[100:300, 100:300]
+    cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 2)
+
+    # Fail Safe Mechanism
+    screen_width, screen_heigh = pyautogui.size()
+    cursor_x, cursor_y = pyautogui.position()
+    if cursor_x >= screen_width - 1 and cursor_y <= 1:
+        console.print("Fail Safe initiated!", style="bold red blink")
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
