3559c3a184868b42b57b8e6478212b1070a803b8
RajVarun <grajvarun@gmail.com>
2024-05-26 01:59:03 +0530

    minor fix

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ console = Console()
 cap = cv2.VideoCapture(0)
 
 # Testing Zone
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
-
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 if not cap.isOpened():
     console.print("Error: Couldn't open winCap", style="bold red")
@@ -18,7 +17,6 @@ while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
-
     # Region to capture
 
     roc = frame[100:300, 100:300]
@@ -26,7 +24,6 @@ while True:
     cv2.rectangle(frame, (100, 100), (300, 300), (255, 0, 0), 2)
 
     cv2.imshow("CamWin", frame)
-
 
     # Fail Safe Mechanism 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -38,7 +35,6 @@ while True:
     if cursor_x >= screen_width - 1 and cursor_y <= 1:
         console.print("Fail-Safe init!", style="bold red blink")
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
