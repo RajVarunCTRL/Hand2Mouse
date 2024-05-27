93213c583944f2edd597c58dded3578f801b5ffa
RajVarun <grajvarun@gmail.com>
2024-05-28 02:25:05 +0530

    Added mouse controls

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import pyautogui
+from pynput.mouse import Button, Controller
 from rich.console import Console
 import mediapipe as mp
 import util
 
 # Getting the screen width and height. Global.
 screen_width, screen_height = pyautogui.size()
+
+
 def findFingerTip(processed):
     if processed.multi_hand_landmarks:
         hand_landmarks = processed.multi_hand_landmarks[0]
@@ -13,24 +16,62 @@ def findFingerTip(processed):
 
     return None
 
+
 def moveMouse(indexFingerTip):
     if indexFingerTip is not None:
         x = int(indexFingerTip.x * screen_width)
         y = int(indexFingerTip.y * screen_height)
-        pyautogui.moveTo(x,y)
+        pyautogui.moveTo(x, y)
+
+
 # Detecting Gestures
+
+# Check for leftclick fucntion.
+def isLeftClick(landmark_list, thumbIndexDist):
+    return util.angleGet(landmark_list[5], landmark_list[6], landmark_list[8]) < 50 < thumbIndexDist and util.angleGet(
+        landmark_list[9], landmark_list[10], landmark_list[12]) < 90
+
+
+# Cehck for rightclick function.
+def isRightClick(landmark_list, thumbIndexDist):
+    return util.angleGet(landmark_list[9], landmark_list[10],
+                         landmark_list[12]) < 50 < thumbIndexDist and util.angleGet(
+        landmark_list[5], landmark_list[6], landmark_list[8]) > 90
+
+
+def isDoubleClick(landmark_list, thumbIndexDist):
+    return util.angleGet(landmark_list[5], landmark_list[6], landmark_list[8]) < 50 < thumbIndexDist and util.angleGet(
+        landmark_list[9], landmark_list[10], landmark_list[12]) > 50
+
+
 def detectGestures(frame, landmarks_list, processed):
-    if len(landmarks_list)>=21:
+    if len(landmarks_list) >= 21:
         # test
         indexFingerTip = findFingerTip(processed)
         thumbIndexDist = util.getDistance([landmarks_list[4], landmarks_list[5]])
 
-        if thumbIndexDist < 50 and util.angleGet(landmarks_list[5], landmarks_list[6], landmarks_list[8])>90:
+        # Mouse ( needs to be optimized )
+
+        if thumbIndexDist < 50 and util.angleGet(landmarks_list[5], landmarks_list[6], landmarks_list[8]) > 90:
             moveMouse(indexFingerTip)
+
+        # Left Clickedey
+        elif isLeftClick(landmarks_list, thumbIndexDist):
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            cv2.putText(frame, "LMB", (150, 150), cv2.FONT_HERSHEY_SIMPLEX, 10, (0, 255, 0), 5)
+
+        elif isDoubleClick(landmarks_list, thumbIndexDist):
+            pyautogui.doubleClick()
+            cv2.putText(frame, "DBDB", (150, 150), cv2.FONT_HERSHEY_SIMPLEX, 10, (255, 0, 0), 5)
+            mouse.press(Button.right)
+            mouse.release(Button.right)
+            cv2.putText(frame, "RMB", (150, 150), cv2.FONT_HERSHEY_SIMPLEX, 10, (255, 0, 0), 5)
 
 
 console = Console()
 cap = cv2.VideoCapture(0)
+mouse = Controller()
 
 mpHands = mp.solutions.hands
 draw = mp.solutions.drawing_utils
@@ -43,8 +84,12 @@ hands = mpHands.Hands(
 )
 
 # Max PROP CAP (WORKS :) )
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)  # Frame Size
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)  # Frame size
+
+actual_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+actual_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(f'Actual frame size: {actual_width}x{actual_height}')  # This depends on the MP of your camera...
 
 if not cap.isOpened():
     console.print("Error: Couldn't open winCap", style="bold red")
